014d34e14c1df77e9371ca1f1cc31d324eef6506
Saad Albadea <122687577+AlbadeaSaad@users.noreply.github.com>
2024-09-25 22:34:31 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,41 @@
 import streamlit as st
+import pandas as pd
+import math
+from PIL import Image
+import numpy as np
+from ultralytics import YOLO
+import cv2
 
-st.title("🎈 Saad app")
-st.write(
-    "Test streamlit "
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='YOLO DETECTOR',
+    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
+
+# -----------------------------------------------------------------------------
+
+# Load the model
+model = YOLO('yolov8n.pt')
+
+# Streamlit app
+st.title("Image Classification with PyTorch")
+
+# File uploader
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Open the uploaded image
+    image = Image.open(uploaded_file).convert("RGB")
+    
+    # Convert to numpy array
+    img_np = np.array(image)
+
+    # Run inference
+    results = model(img_np)
+
+    # Display the results
+    #st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # Visualize results
+    annotated_frame = results[0].plot()  # Get the annotated image
+    st.image(annotated_frame, caption='Detection Results', use_column_width=True)
