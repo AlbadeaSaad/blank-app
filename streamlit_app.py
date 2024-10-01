abebfe3269e97c8368c6afb88ac0c0fa80b245ef
Saad Albadea <122687577+AlbadeaSaad@users.noreply.github.com>
2024-10-01 18:48:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,48 +21,35 @@ model = YOLO("best.pt")
 st.title("Image Classification with PyTorch")
 
 # File uploader
-uploaded_file = st.file_uploader('choose a video...', type=['mp4'])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Get the file name
-    file_name = uploaded_file.namedd
-
-cap = cv2.VideoCapture(file_name+".mp4")
-w, h, fps = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT), cap.get(cv2.CAP_PROP_FPS)
-
-width = 640
-height = 480
-
-# Create a VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Replace with your desired codec
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width, height))
-
-while cap.isOpened():
-    # Read a frame from the video
-    success, frame = cap.read()
-
-    if success:
-        # Run YOLOv8 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=False)
-        out.write(results)
-out.release()
-cap.release()
-#uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-#if uploaded_file is not None:
     # Open the uploaded image
-    #image = Image.open(uploaded_file).convert("RGB")
+    image = Image.open(uploaded_file).convert("RGB")
     
     # Convert to numpy array
-    #img_np = np.array(image)
+    img_np = np.array(image)
 
     # Run inference
-    #results = model(img_np)
+    results = model(img_np)
 
     # Display the results
     #st.image(image, caption='Uploaded Image', use_column_width=True)
 
     # Visualize results
-    #annotated_frame = results[0].plot()  # Get the annotated image
+    annotated_frame = results[0].plot()  # Get the annotated image
+    st.image(annotated_frame, caption='Detection Results', use_column_width=True)
+#uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Convert to numpy array
+    img_np = np.array(image)
+
+    # Run inference
+    results = model(img_np)
+
+    # Display the results
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # Visualize results
+    annotated_frame = results[0].plot()  # Get the annotated image
 #video_bytes = out.read()
-st.video(out)
+#st.video(out)
